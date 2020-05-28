5748c758c9013902ebdef42782040569025911da
*** Haley Harris *** <*** hbharris94@gmail.com ***>
2020-05-27 22:00:23 -0500

    password locker program

---
+++ csc221/chap6/pw.py
@@ -0,0 +1,19 @@
+# Chapter 6: insecure password locker program
+
+PASSWORDS = {'email': 'F7mip4SDX88DAsdf42jB586v',
+             'blog': 'adleB6dRn78ld02lsnBCv',
+             'luggage': '12345'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password.')
+    sys.exit()
+
+# first command line argument is account name
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
